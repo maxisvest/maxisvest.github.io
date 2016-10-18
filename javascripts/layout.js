d19aaaf5aeba1458b2d74e5acf13f4984b64e95d
maxisvest <1447829379@qq.com>
2016-10-18 11:14:06 +0800

    0.22c

--- javascripts/layout.js
+++ javascripts/layout.js
@@ -11,6 +11,15 @@ function easter_egg(){
     }
 
 $(document).ready(function(){
-    $("#planetPic").animate({top:'-=100px',
-    opacity:'1'},1500);
-});	
+    setTimeout(function(){$("#planetPic").animate({top:'-=50px', opacity:'1'},1000);},600);
+    setTimeout(function(){$("#1").animate({opacity:'1'},400);},0);
+    setTimeout(function(){$("#2").animate({opacity:'1'},400);},200);
+    setTimeout(function(){$("#3").animate({opacity:'1'},400);},400);
+    setTimeout(function(){$("#4").animate({opacity:'1'},400);},600);
+    setTimeout(function(){$("#5").animate({opacity:'1'},400);},800);
+    setTimeout(function(){$("#6").animate({opacity:'1'},400);},1000);
+    setTimeout(function(){$("#7").animate({opacity:'1'},2000);},0);
+    setTimeout(function(){$("#foot_hr").animate({width:'100%'},2000);},0);
+    setTimeout(function(){$("#project_title").animate({top:'+=220', opacity:'1'},1400);},0);
+    setTimeout(function(){$("#project_tagline").animate({top:'+=240', opacity:'1'},1400);},200);
+});
